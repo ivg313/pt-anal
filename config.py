e22eac54eabada548659e1e3dd29194773d7ed34
Igor Grigoryev <ivg313@gmail.com>
2023-02-01 02:34:20 +0300

    minor fix

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ apiToken = '' # Your bot's Telegram API token
 chatID = '' # Telegram chat ID
 
 web = [ # array of arrays what http(s) should be checked
-    ['Google', 'https://google.com/', 'Google Search'], # Human readable name, URL, keyword or keyphrase
+    ['Google', 'https://google.com/', '</html>'], # Human readable name, URL, keyword or keyphrase
     ['Bing', 'https://bing.com/', 'search_icon'], # and another ...
 ]
